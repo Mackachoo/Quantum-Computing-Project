c4795e663bdec8a0b8f05ddc09c7c15ec163694d
Mackachoo <Mackachooo@gmail.com>
2020-03-24 20:12:00 +0000

    Graph stuff

--- efficiency.py
+++ efficiency.py
@@ -4,13 +4,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Run for different sizes of registers and see how time scales for each algorithm f
-def check_efficiency(f):
+
+def writeData(n, f, file):
+    s = 1
+    Dt = []
+    DtS = []
+    nq = np.arange(n)+1
+
+    store = open(file, 'a')
+    store.write(str(nq))
+    store.close()
+
+    for n in nq:
+        #Run Grover's algorithm for the given parameters
+        print(f"Running Grover's, {int(np.pi/(4*np.arcsin(1/np.sqrt(2**n))))} times:")
+        start_time = time.time()
+        R, t = f(n,s,False)
+        Dt.append( time.time() - start_time)
+        store = open(file, 'a')
+        store.write(f"\n\n{Dt}")
+        store.close()
+
+        start_time = time.time()
+        R, t = f(n,s,False,True)
+        DtS.append( time.time() - start_time)
+        store = open(file, 'a')
+        store.write(f"\n{DtS}")
+        store.close()
+
+
+def getData(file):
+    f = open(file, 'r')
+    d = f.read()
+    f.close()
+    data = d.split('\n')
+
+    nq = np.array([float(a) for a in (data[0])[1:-1].split(' ')])
+    Dt = np.array([float(a) for a in (data[1])[1:-1].split(', ')])
+    DtS = np.array([float(a) for a in (data[2])[1:-1].split(', ')])
+    print(f"{nq}\n{Dt}\n{DtS}")
+    return nq, Dt, DtS
+
+
+def check_efficiency(data):
     """Short summary.
+
 
     Parameters
     ----------
-    f : type
-        Description of parameter `f`.
+    f : str
+        String for filename with data.
 
     Returns
     -------
@@ -18,37 +61,69 @@ def check_efficiency(f):
         Description of returned object.
 
     """
-    s = 1
-    Dt = []
-    DtS = []
-    T = []
-    nq = np.arange(9)+1
-    print(nq)
+    
+    nq, Dt, DtS = data
+    fig = plt.figure()
+    dd = fig.add_subplot(111)
+    #f1 = fig.add_subplot(311)
+    #f2 = fig.add_subplot(323)
+    #f3 = fig.add_subplot(324)
+    #f4 = fig.add_subplot(325)
+    #f5 = fig.add_subplot(326)
 
-    wr = open("Time_vs_N.txt","w")
-    wr.write("N, Time taken for Grover's only, Total time taken (to construct gates and run Grover's)" + '\n')
+    #nq = 2**np.array(nq)
 
-    for n in nq:
-        #Run Grover's algorithm for the given parameters
-        print(f"Running Grover's, {int(np.pi/(4*np.arcsin(1/np.sqrt(2**n))))} times:")
-        start_time = time.time()
-        R, t = f(n,s,False)
-        T.append(t)
-        Dt.append( time.time() - start_time)
+    dd.set_title("Log of Time vs qbits per state for Sparse and Non Sparse Method")
+    dd.plot(nq, Dt*1000, label="Non-Sparse Method")
+    dd.plot(nq, DtS*1000, label="Sparse Method")
+    dd.legend()
+    dd.set_yscale('log')
+    dd.set_xlabel("Qbits in Register")
+    dd.set_ylabel("Log of Time")
+    dd.set_ylim((0,max(abs(Dt*1000))))
 
-        start_time = time.time()
-        R, t = f(n,s,False,True)
-        T.append(t)
-        DtS.append( time.time() - start_time)
-        wr.write("%f %10.10f\n" % (t, time.time() - start_time))
 
-    nq = 2**nq
-    wr.close()
-    plt.plot(nq, Dt, label="Not Sparse")
-    plt.plot(nq, DtS, label="Sparse")
-    #plt.plot(nq, T)
-    plt.legend()
-    plt.yscale('log')
     plt.show()
 
-check_efficiency(gr.Grovers)
+    
+    """
+    f1.set_title("Time Difference")
+    f1.plot(nq, abs(np.array(Dt)-np.array(DtS))*1000, label="Not Sparse")
+    f1.set_yscale('log')
+    f1.set_xlabel("qbits per state")
+    f1.set_ylabel("Time (ms)")
+    f1.set_ylim((0,max(abs(np.array(Dt)-np.array(DtS))*1000)))
+
+    f2.set_title("Non Sparse Log Time")
+    f2.plot(nq, Dt*1000, label="Not Sparse")
+    f2.set_yscale('log')
+    f2.set_xlabel("qbits per state")
+    f2.set_ylabel("Time (ms)")
+    f2.set_ylim((0,max(abs(Dt*1000))))
+
+    f3.set_title("Sparse Log Time")
+    f3.plot(nq, DtS*1000, label="Sparse")
+    f3.set_yscale('log')
+    f3.set_xlabel("qbits per state")
+    f3.set_ylabel("Time (ms)")
+    f3.set_ylim((0,max(DtS*1000)))
+
+    f4.set_title("Non Sparse Time")
+    f4.plot(nq, Dt*1000, label="Not Sparse")
+    f4.set_xlabel("qbits per state")
+    f4.set_ylabel("Time (ms)")
+    f4.set_ylim((0,max(abs(Dt*1000))))
+
+    f5.set_title("Sparse Time")
+    f5.plot(nq, DtS*1000, label="Sparse")
+    f5.set_xlabel("qbits per state")
+    f5.set_ylabel("Time (ms)")
+    f1.set_ylim((0,max(DtS*1000)))
+
+    """
+
+file = "data1.txt"
+
+#writeData(50, gr.Grovers, file)
+check_efficiency(getData(file))
+
